f84fb037175adf65c4f4da1e7bffb615163ea10d
Ripon Datta <ripondatta.us@gmail.com>
2019-06-11 22:11:45 -0700

    Add project files.

---
+++ CheckBSTOrNot/BinaryTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckBSTOrNot
+{
+    public class BinaryTree
+    {
+        public Node Root;
+  
+        public bool IsBinarySeachTree()
+        {
+            return IsBinarySeachTree(Root, int.MinValue, int.MaxValue);
+        }
+
+        private bool IsBinarySeachTree(Node root, int min, int max)
+        {
+            if(root == null)
+            {
+                return true;
+            }
+
+            if(root.Data < min || root.Data > max)
+            {
+                return false;
+            }
+
+            return IsBinarySeachTree(root.Left, min, root.Data - 1) && IsBinarySeachTree(root.Right, root.Data + 1, max);
+        }
+    }
+}
---
+++ CheckBSTOrNot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckBSTOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree1 = new BinaryTree();
+            tree1.Root = new Node(10);
+            tree1.Root.Left = new Node(8);
+            tree1.Root.Right = new Node(2);
+            tree1.Root.Left.Left = new Node(3);
+            tree1.Root.Left.Right = new Node(5);
+            tree1.Root.Right.Left = new Node(9);
+            tree1.Root.Right.Right = new Node(10);
+            tree1.Root.Right.Right.Right = new Node(16);
+
+            var isBST = tree1.IsBinarySeachTree();
+            Console.WriteLine(isBST);
+
+            BinaryTree tree2 = new BinaryTree();
+            tree2.Root = new Node(10);
+            tree2.Root.Left = new Node(5);
+            tree2.Root.Right = new Node(15);
+            tree2.Root.Left.Left = new Node(2);
+            tree2.Root.Left.Right = new Node(8);
+            tree2.Root.Right.Right = new Node(20);
+
+            isBST = tree2.IsBinarySeachTree();
+            Console.WriteLine(isBST);
+        }
+    }
+}
